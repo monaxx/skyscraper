c9feb30978f52fef3b3c8e13bab8a3984e190381
Raymond Inson <66734118+monaxx@users.noreply.github.com>
2023-06-08 14:59:44 +0800

    Update SolveSkyscraper.controller.php

--- api/controllers/SolveSkyscraper.controller.php
+++ api/controllers/SolveSkyscraper.controller.php
@@ -1,6 +1,15 @@
 <?php
     include 'inc/classAutoloader.inc.php';
     function solveSkyscraper($sideValues){
+        // Suggestion ni Titi:
+        // $table = new Table();
+        //
+        // Dari mahitabo tong pag create sa 2D array and 
+        // pag add adtong mga side values
+        // $table->initializeTabe($sideValues);
+        //
+        // $solution = $table->solveProblem();
+        //
         $solution = new Table($sideValues);
         return $solution->getSolution();
     }
